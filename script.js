6b881ab401ae18c84db914356678b7cccff62f66
Tejaswini <tejaswinipichala@gmail.com>
2023-07-04 21:37:58 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -1,45 +1,29 @@
-const celsiusInput = document.querySelector('#celsius > input');
-const fahrenheitInput = document.querySelector('#fahrenheit > input');
-const kelvinInput = document.querySelector('#kelvin > input');
 
-const roundToTwoDP = (num) => {
-  return num.toFixed(2);
-};
+    const calculateTemp = () =>{
+        let temp = document.getElementById('temp').value;
+        // console.log(temp);
 
+        let units = document.getElementById('units');
+        const unitsType = units.options[units.selectedIndex].value;
+        // console.log(unitsType);
 
-const celsiusToFaAndKe = () => {
-  const celsiusTemp = parseFloat(celsiusInput.value);
-  const fahrenheitTemp = (celsiusTemp * 1.8) + 32;
-  const kelvinTemp = celsiusTemp + 273.15;
+        const celToFah = (cel) =>{
+            let fahrenheit = Math.round((cel * 9/5) + 32);
+            return fahrenheit;
+        }
 
-  fahrenheitInput.value = roundToTwoDP(fahrenheitTemp);
-  kelvinInput.value = roundToTwoDP(kelvinTemp);
-};
+        const fahToCel = (feh) =>{
+            let celsius = Math.round((feh - 32) * 5/9);
+            return celsius;
+        }
+        let result;
 
+        if(unitsType == 'cel'){
+            result = celToFah(temp);
+            document.querySelector('.result').innerHTML = `${result}°Fahreheit`;
+        }else{
+            result = fahToCel(temp);
+            document.querySelector('.result').innerHTML = `${result}°Celsius`;
+        }
 
-const fahrenheitToCeAndKe = () => {
-  const fahrenheitTemp = parseFloat(fahrenheitInput.value);
-  const celsiusTemp = (fahrenheitTemp - 32) * (5 / 9);
-  const kelvinTemp = (fahrenheitTemp + 459.67) * (5 / 9);
-
-  celsiusInput.value = roundToTwoDP(celsiusTemp);
-  kelvinInput.value = roundToTwoDP(kelvinTemp);
-};
-
-const kelvinToCeAndFa = () => {
-  const kelvinTemp = parseFloat(kelvinInput.value);
-  const celsiusTemp = kelvinTemp - 273;
-  const fahrenheitTemp =  1.8 * (kelvinTemp - 273) + 32;
-
-  celsiusInput.value = roundToTwoDP(celsiusTemp);
-  fahrenheitInput.value = roundToTwoDP(fahrenheitTemp);
-};
-
-
-const main = () => {
-  celsiusInput.addEventListener('input', celsiusToFaAndKe);
-  fahrenheitInput.addEventListener('input', fahrenheitToCeAndKe);
-  kelvinInput.addEventListener('input', kelvinToCeAndFa);
-};
-
-main();
+    };
